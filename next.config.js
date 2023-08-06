a6fff63e52b41b04c4967d7b2c060e32869fdae6
tccmd <tccmd52@gmail.com>
2023-08-06 19:58:55 +0900

    0806 7:58

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: (() => {
+    let compilerConfig
+    // let compilerConfig = {
+    //   styledComponents: true
+    // }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+      reactRemoveProperties: { properties: ['^data-testid$']}
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
